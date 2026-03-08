c171af72af958f2b222bfcb1eeea623ea59b92eb
abdulhadi-143 <24220400055@mersin.edu.tr>
2026-03-08 10:37:53 +0300

    feat(descriptive): implement sample_variance and sample_std functions - calculate variance and standard deviation from sample data

--- src/01_descriptive.py
+++ src/01_descriptive.py
@@ -23,3 +23,35 @@ numpy_mean=np.mean(yields)
 print(f"My mean: {my_mean}")
 print(f"Numpy mean: {numpy_mean}")
 
+
+
+"""Now we will see another important function in statistics that is VARIANCE and formula for the variance is s² = Σ(xᵢ - x̄)² / (n - 1)
+So lets try to prove our results by matching the results shown by using numpy std function"""
+
+def sample_variance(data:list):
+    #calculated the variance of the given sample.
+    n = len(data)
+    mean = sample_mean(data)
+    sum_of_squares = 0
+    for x in data:
+        diff = x - mean
+        squared = diff ** 2
+        sum_of_squares = sum_of_squares + squared
+    return sum_of_squares / (n - 1)
+
+import numpy as np
+
+numpy_variance=np.var(yields, ddof=1)
+
+print(f"My variance: {sample_variance(yields)}") 
+print(f"Numpy variance: {numpy_variance}")
+
+
+
+"The procedure for the standard deviation is to take the square root of the variance and the formula is s = sqrt(s²) where s² is the variance"
+import math
+def sample_std(data: list):
+    return math.sqrt(sample_variance(data))  #or return sample_variance(data) ** 0.5
+
+print(f"My std dev    : {sample_std(yields)}")
+print(f"Numpy std dev : {np.std(yields, ddof=1)}")
